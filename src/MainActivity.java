f42f1d122d73b810663f2b694627a0ece15c48cc
Jimmy Le <jimmylle@yahoo.com>
2015-07-31 17:55:35 -0700

    Gets every LN on BakaTsuki main project page including the url

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -1,10 +1,8 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.safety.Whitelist;
 import org.jsoup.select.Elements;
 
-import java.awt.*;
 import java.io.*;
 
 /**
@@ -13,31 +11,29 @@ import java.io.*;
 public class MainActivity {
 
     public static void main(String[] args) {
-        for (int i = 1; i < 4; i++) {
-            for (int j = 1; j < 4; j++) {
-                try {
-                    File file = new File("C:\\Users\\jimmy\\Desktop\\test-v" + i + "-c" + j + ".doc");
+        try {
+            File file = new File("C:\\Users\\jimmy\\Desktop\\test-v1-c1.doc");
 
-                    if (!file.exists()) {
-                        file.createNewFile();
-                    }
-                    String url = "http://royalroadweed.blogspot.co.il/2014/11/volume-" + i;
-                    url += "-chapter-" + j + ".html";
-                    Writer writer = new BufferedWriter(new FileWriter(file));
-                    Document document = Jsoup.connect(url).get();
-                    Elements span = document.select("span[style*=background-color]");
-                    Desktop desktop = Desktop.getDesktop();
-                    //desktop.open(file);
-                    for (Element s : span) {
-                        String text = Jsoup.clean(s.text(), Whitelist.basic());
-                        writer.write(text + "\r\n\r\n");
-                    }
-                    writer.flush();
-                    writer.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            if (!file.exists()) {
+                file.createNewFile();
             }
+            String url = "http://www.baka-tsuki.org/project/index.php?title=Category:" +
+                    "Light_novel_(English)";
+            Writer writer = new BufferedWriter(new FileWriter(file));
+            Document document = Jsoup.connect(url).get();
+            Elements elements = document.select("div#mw-pages a[title]");
+
+            for (Element element: elements) {
+                String title = element.text();
+                String abs_url = element.attr("abs:href");
+                title += "\r\n" + abs_url + "\r\n\r\n";
+                writer.write(title);
+            }
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+
         }
     }
 }
