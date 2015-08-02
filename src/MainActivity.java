985032576911fd2336ea124f2de175d03b60857b
Jimmy Le <lejimmyl@yahoo.com>
2015-08-01 20:43:22 -0700

    Able to print out chapters of a specifi volume

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -1,9 +1,15 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by jimmy on 7/31/2015.
@@ -12,10 +18,11 @@ import java.io.*;
 public class MainActivity {
 
     private static String url = "http://www.baka-tsuki.org/project/index.php?title=";
+    private static String api = "https://baka-tsuki-api.herokuapp.com/api?title=";
 
     public static void main(String[] args) {
         //listLightNovels();
-        findSpecificLN("Moonlight_Sculptor");
+        findSpecificLN("Moonlight_Sculptor", 1);
     }
 
     public static void listLightNovels() {
@@ -44,15 +51,37 @@ public class MainActivity {
         }
     }
 
-    private static void findSpecificLN(String title) {
+    private static void findSpecificLN(String title,int volume) {
         try {
             File file = new File("C:\\Users\\jimmy\\Desktop\\test.txt");
             if (!file.exists()) {
                 file.createNewFile();
             }
-            Document document = Jsoup.connect(url + cleanLightNovelTitle(title)).get();
             Writer writer = new BufferedWriter(new FileWriter(file));
+            //Connect to the URL
+            URL url = new URL(api + cleanLightNovelTitle(title) + "&volumeno=" + volume);
+            HttpURLConnection request = (HttpURLConnection) url.openConnection();
+            request.connect();
 
+            //Convert to JSON to get info
+            JsonParser jsonParser = new JsonParser();
+            JsonElement root = jsonParser.parse(new InputStreamReader((InputStream) request.getContent()));
+            String jsonString = root.toString();
+            JsonElement jsonElement = new JsonParser().parse(jsonString);
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
+            JsonArray jsonArray = jsonObject.getAsJsonArray("sections");
+            jsonObject = jsonArray.get(0).getAsJsonObject();
+            jsonArray = jsonObject.getAsJsonArray("books");
+            jsonObject = jsonArray.get(0).getAsJsonObject();
+            jsonArray = jsonObject.getAsJsonArray("chapters");
+            String result;
+            for (int i = 0; i < jsonArray.size(); i++) {
+                jsonObject = jsonArray.get(i).getAsJsonObject();
+                result = jsonObject.get("title").toString();
+                writer.write(result + "\r\n\r\n");
+            }
+            //String size = Integer.toString(jsonArray.size());
+            //writer.write(size);
             writer.flush();
             writer.close();
         } catch (IOException e) {
